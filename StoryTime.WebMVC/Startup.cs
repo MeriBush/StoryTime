760b3bfbee636dcce1fb5e0eecbe8676c6140423
Merideth Bush <meridethbush@gmail.com>
2018-08-20 11:27:18 -0400

    Add project files.

---
+++ StoryTime.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StoryTime.WebMVC.Startup))]
+namespace StoryTime.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
